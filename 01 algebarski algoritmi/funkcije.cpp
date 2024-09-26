e5d6114b10bb0e3533ae20aa5576c549e1773803
SavaVg <sava.maksimovic@valjevskagimnazija.edu.rs>
2024-09-26 13:12:52 +0200

    Create funkcije.cpp

---
+++ 01 algebarski algoritmi/funkcije.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long unsigned llu;
+
+bool jeProst(llu n){
+    if (n==2) return true;
+    if(n==1 || n%2==0) return false;
+    llu i=3;
+    while(i*i<=n) {
+        if (n%i==0) return false;
+        i+=2;
+    }
+    return true;
+}
+
+void faktorisi(llu ulaz, map<llu, int>& izlaz) {
+    llu i=2;
+    while (i*i<=ulaz) {
+            while (ulaz%i==0) {
+                izlaz[i]++;
+                ulaz /= i;
+            }
+            i++;
+    }
+
+    if (ulaz!=1) izlaz[ulaz]++;
+}
+
+void nzd(map<llu, int>& fa, map<llu, int>& fb, map<llu, int>& fnzd){
+    for (auto x: fa) if (fb[x.first]!=0) fnzd[x.first] = (fb[x.first]<=x.second ? fb[x.first] : x.second);
+}
+
+void nzs(map<llu, int>& fa, map<llu, int>& fb, map<llu, int>& fnzs){
+    fnzs=fa;
+    for (auto x: fb) if (x.second > fa[x.first])  fnzs[x.first] = x.second;
+}
+
+void ispisi(map<llu, int> m){
+    for (auto x: m) cout << x.first << ' '<< x.second << endl;
+}
